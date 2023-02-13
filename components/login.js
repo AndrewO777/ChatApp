bc3350914e3f9a107469737e44c1411f12f17651
cjl1197 <cjl1197@gmail.com>
2023-02-13 15:13:14 -0500

    added login.js

---
+++ components/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, tInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+export default function Login() {
+
+
+    return (
+
+        <View style={styles.container}>
+    
+            <Text>Login</Text>
+            <TextInput></TextInput> 
+        </View>
+)}
