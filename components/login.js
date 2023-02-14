5aada58fd9c7cbf3be61c78d4393b49b8398fb9a
cjl1197 <cjl1197@gmail.com>
2023-02-13 22:24:35 -0500

    login update

--- components/login.js
+++ components/login.js
@@ -9,10 +9,13 @@ export default function Login() {
     return (
 
     <TouchableOpacity>
+        <View>
+            <Text style={styles.welcome}>Welcome Back</Text>
+        </View>
         <View style={styles.container}>
-                <TextInput style={styles.input} placeholder=" Username" />
-                <TextInput style={styles.input} placeholder=" Password" />
-                            <Button style={styles.button} title="Login" />
+            <TextInput style={styles.input} placeholder=" Username" />
+            <TextInput style={styles.input} placeholder=" Password" />
+            <Button style={styles.button} title="Login" />
         </View>
         <View style={styles.signUpContainer}>
             <Text>Don't have an account? 
@@ -32,28 +35,27 @@ container: {
     justifyContent: 'center',
     flex: 1,
 },
+welcome: {
+    fontSize: 40,
+    marginTop: 80,
+    fontWeight: 'bold',
+},
 input: {
     height: 40,
     width: 250,
     borderColor: '#000',
     borderWidth: 2,
     borderRadius: 15,
-    margin: 10,
+    margin: 20,
 },
 signUpContainer: {
+    marginTop: 20,
     marginBottom: 20,   
+    marginLeft: 40,
 },
 signUp: {
     color: '#0000FF',
     marginLeft: 10,
-},
-button: {
-    backgroundColor: '#fff',
-    borderColor: '#0000FF',
-    borderWidth: 2,
-    width: 200,
-    height: 40,
-    borderRadius: 15,
 },
 text: {
     marginLeft: 10,
