77a8a15646c3450bb6a8f673cb3de89eff9aa62c
cjl1197 <cjl1197@gmail.com>
2023-03-02 00:30:24 -0500

    input covered by keyboard fixed

--- components/screens/chatpage.js
+++ components/screens/chatpage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
-import { ScrollView, View, Text, TextInput, StyleSheet, TouchableHighlight, FlatList, Image } from "react-native";
+import { ScrollView, View, Text, TextInput, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, FlatList, Image, KeyboardAvoidingView, Keyboard, SafeAreaView} from "react-native";
 import { GlobalContext } from "../../globalContext";
 import { firebase, hooks } from "../firebase/firebase";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 export default function ChatPage({navigation,route}){
@@ -30,6 +30,7 @@ export default function ChatPage({navigation,route}){
 		} catch (error) {
 			console.log(error);
 			}
+			setMessage('')
 
 	}
 
@@ -53,43 +54,77 @@ export default function ChatPage({navigation,route}){
 
 		navigation.setOptions({ title: "Chatting With "+user.name });
 	},[user,navigation]);
+
+
+
 	return (
-		<View style={{ flex: 1 }}>
+	
+	
+		
+		
+		
+		<View style={{flex: 1, backgroundColor: '#c0c0c0'}}>
 			<FlatList
 				data = { messages }
 				renderItem = {({ item }) => (
 					<View style={ item.sender == userID ? styles.itemMe : styles.item }>
-					{ item.sender != userID ? <Image 
-		source={{ width:50,height:50,uri:"https://picsum.photos/50"}} style={ styles.picture }/>
-						: null }
-						<Text>{ item.message }</Text>
+							{ item.sender != userID ? <Image 
+							source={{ width:40,height:40,uri:"https://picsum.photos/50"}} style={ styles.picture }/>
+							: null }
+								<Text style={{color: 'white', fontSize: '20%'}}>{ item.message }</Text>
 					</View>
 				)}
 				keyExtractor={(item) => item.id}
 			/>
+				<KeyboardAvoidingView 
+					behavior="position"
+					keyboardVerticalOffset={60}
+				>
+					<View style={ styles.inputWrapper }>
+						{/* <View style={styles.addFileBtn}>
+							<AntDesign
+								name='pluscircle'
+								size={50}
+								onPress={() => console.log('pressed')}
+							/>
+						</View> */}
+							<TextInput
+								style={ styles.input }
+								onChangeText={(val) => setMessage(val)}
+								clearButtonMode='always'
+								value={message}
+								selectionColor='white'
+								
 
-			<View style={ styles.inputWrapper }>
-			{/* <View style={styles.addFileBtn}>
-				<AntDesign
-					name='pluscircle'
-					size={50}
-					onPress={() => console.log('pressed')}
-				/>
-			</View> */}
-				<TextInput 
-					style={ styles.input }
-					onChangeText={(val) => setMessage(val)}
-				/>
-				<TouchableHighlight style={ styles.button }
-					onPress={sendMessage}>
-					<Text style={{ color: "#fff" }}>Send</Text>
-				</TouchableHighlight>
-			</View>
+							/>
+							<MaterialCommunityIcons
+								name='send-circle'
+								color='#3d3d3d'
+								size= '60%'
+								onPress={() => 
+									{sendMessage()
+									Keyboard.dismiss()}
+								}
+							/>
+							
+							{/* <TouchableHighlight style={ styles.button }
+								onPress={sendMessage}>
+								<Text style={{ color: "#fff" }}>Send</Text>
+							</TouchableHighlight> */}
+							
+					</View>
+				</KeyboardAvoidingView>
 		</View>
+						
+						
+			
 	);
 }
 
 const styles = StyleSheet.create({
+// container: {
+// 	flex: 1
+// },
 inputWrapper: {
 	position: "absolute",
 	flexDirection: "row",
@@ -97,35 +132,40 @@ inputWrapper: {
 	left: 0,
 	right: 0,
 	padding: 10,
-	alignItems: "center"
+	alignItems: "center",
+	backgroundColor: "#c0c0c0"
 },
 input: {
     height: 55,
-    width: 250,
+    width: '80%',
     borderColor: '#000',
     borderWidth: 2,
     borderRadius: 30,
     paddingLeft: 10,
-    margin: 20,
-    marginRight: 10
+    margin: 0,
+    marginRight: 10,
+	color: '#fff',
+	backgroundColor: "#3d3d3d",
+	fontSize: '20%'
 },
 button: {
 	backgroundColor: "#00aeef",
+	width: '20%',
 	borderRadius: 15,
 	padding: 15,
 	paddingLeft: 20,
 	paddingRight: 20
 },
 item: {
-	padding: 10,
+	padding: 5,
 	margin: 5,
 	marginLeft: 10,
 	marginRight: 10,
 	flexDirection: "row",
 	alignItems: "center",
 	alignSelf: "start",
-	backgroundColor: "#00aeef",
-	borderRadius: 50
+	backgroundColor: "#476a6f",
+	borderRadius: 50,
 },
 itemMe: {
 	padding: 10,
@@ -136,8 +176,8 @@ itemMe: {
 	alignItems: "center",
 	alignSelf: "flex-end",
 	justifyContent: "right",
-	backgroundColor: "green",
-	borderRadius: 50
+	backgroundColor: "#7eb09b",
+	borderRadius: 50,
 },
 picture: {
 	marginRight: 10,
